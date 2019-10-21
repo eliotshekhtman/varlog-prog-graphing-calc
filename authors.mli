0675eea68a2c9317b1b5e917dddede6439e984dd
ess239 <ess239@cornell.edu>
2019-10-20 22:18:19 -0400

    Initialization implementation

---
+++ authors.mli
@@ -0,0 +1,9 @@
+(** 
+   Metadata about this submission.
+   @author Eliot Seo Shekhtman (ess239), Omar Bekdash (ob47), 
+   Adith Ramachandran (ar729)
+*)
+
+(** [hours_worked] is a list of the number of hours each team member
+    worked on this assignment. *)
+val hours_worked : int list
