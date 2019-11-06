ef69b9c6334ad4e1d6973f55a23844ca1c1ef240
ess239 <ess239@cornell.edu>
2019-11-06 11:36:37 -0500

    Made funky and sneaky

--- repl.ml
+++ repl.ml
@@ -1,9 +1,10 @@
 open Main 
+open Graphing
 
 let rec run () =
   print_string "> ";
   match read_line () with
-  | exception (Graphics.Graphic_failure _) -> run ()
+  | exception (Graphics.Graphic_failure _) -> print_endline "Quitting"
   | "" -> run ()
   | "QUIT" -> print_endline "Quitting"
   | "SET_SCALE" -> begin print_endline "x" end
@@ -16,4 +17,15 @@ let rec run () =
       | s -> print_endline s; run ()
     end
 
-let () = run ()
+let flex () = 
+  print_endline "------------ V/\\RL0G -------------";
+  print_endline "[EVAL e] to evaluate an expression";
+  print_endline "[GRAPH f] to graph a function";
+  print_endline "  (implicitly set to y)";
+  print_endline "  Do not close the graphing window";
+  print_endline "----------------------------------"; ()
+
+let () = 
+  graph_func (-10.) 10. (-10.) 10. 0 (fun x -> 100000.);
+  flex ();
+  run ()
