e15734565e8734476056f7c17b4c8b130fe78e22
ess239 <ess239@cornell.edu>
2019-11-05 23:19:56 -0500

    Added sneaky snek

--- graphing.ml
+++ graphing.ml
@@ -9,6 +9,14 @@ let better_int_of_float (f : float) =
   else if f <= (min_int |> float_of_int) then min_int / 2
   else f |> int_of_float
 
+let setup () = 
+  let _ = open_graph "" in
+  let _ = set_window_title "Graph 2-D Functions" in
+  let window_height = 800 in
+  let window_width = 800 in
+  let _ = resize_window window_width window_height in
+  ()
+
 (** [graph_func xi xa yi ya x f] is [unit], but it graphs function f, 
     starting at pixel [x], with bounds min x = [xi], max x = [xa],
     min y = [yi], max y = [ya]. *)
